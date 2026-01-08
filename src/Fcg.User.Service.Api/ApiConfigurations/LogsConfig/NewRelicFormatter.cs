3679576816ac2a75142421d0f5bc185358c44365
marcos lancy <marcos.system32@gmail.com>
2026-01-08 15:52:44 -0300

    adiciona namespace

--- src/Fcg.User.Service.Api/ApiConfigurations/LogsConfig/NewRelicFormatter.cs
+++ src/Fcg.User.Service.Api/ApiConfigurations/LogsConfig/NewRelicFormatter.cs
@@ -15,7 +15,7 @@ public class NewRelicFormatter : ITextFormatter
             level = logEvent.Level.ToString(),
             attributes = new
             {
-                app = "Fcg",
+                app = "Fcg User Service",
                 source = logEvent.Properties.ContainsKey("SourceContext") ? logEvent.Properties["SourceContext"].ToString().Trim('"') : null,
                 exception = logEvent.Exception?.ToString()
             }
